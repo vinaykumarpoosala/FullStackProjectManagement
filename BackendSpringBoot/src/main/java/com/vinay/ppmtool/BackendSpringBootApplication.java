bee3a3bd85fef26b6fbe01fa42e0946eb57c1aee
= <->
2020-09-13 10:33:40 +0530

    first commit

---
+++ BackendSpringBoot/src/main/java/com/vinay/ppmtool/BackendSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.vinay.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringBootApplication.class, args);
+	}
+
+}
