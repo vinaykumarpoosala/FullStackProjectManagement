0ff07fc4c13080f00d24cdfcbdcacefaa352d016
= <->
2020-10-04 12:28:26 +0530

    seperate code base for react and spring

--- BackendSpringBoot/src/main/java/com/vinay/ppmtool/web/ProjectController.java
+++ BackendSpringBoot/src/main/java/com/vinay/ppmtool/web/ProjectController.java
@@ -1,7 +1,7 @@
 package com.vinay.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.security.Principal;
 
 import javax.validation.Valid;
 
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,27 +20,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vinay.ppmtool.domain.Project;
 import com.vinay.ppmtool.service.ProjectService;
+import com.vinay.ppmtool.service.ValidationErrorMapService;
 
 @RestController
 @RequestMapping("/api/projects")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class ProjectController {
 
 	@Autowired
 	private ProjectService service;
-	
+
+	@Autowired
+	private ValidationErrorMapService errorService;
+
 	@PostMapping("")
-	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project , BindingResult result){
-		if(result.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			result.getFieldErrors()
-					.stream()
-					.forEach(err -> {	
-				errorMap.put(err.getField(), err.getDefaultMessage());
-			});
-			return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
-		}
-		
-		Project project1 = service.saveOrUpdate(project);
-		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result ,
+			Principal principal) {
+
+		ResponseEntity<?> errorMap = errorService.mapForErrors(result);
+		if (errorMap != null)
+			return errorMap;
+		Project project1 = service.saveOrUpdate(project,principal.getName());
+		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 	}
+
+	@GetMapping("/{projectId}")
+	public ResponseEntity<?> findProject(@PathVariable String projectId , Principal principal) {
+
+		return new ResponseEntity<Project>(service.findProjectByIdentifier(projectId , principal.getName()), HttpStatus.OK);
+
+	}
+
+	@GetMapping("/all")
+	public Iterable<Project> findAllProjects(Principal principal) {
+
+		return service.findAllProjects(principal.getName());
+	}
+
+	@DeleteMapping("/{projectId}")
+
+	public ResponseEntity<?> deleteProject(@PathVariable String projectId , Principal principal) {
+		service.deleteProject(projectId , principal.getName());
+		return new ResponseEntity<String>("Project with Id '" + projectId + "' has been deleted", HttpStatus.OK);
+	}
+
 }
